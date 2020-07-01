dcc023f293da5867c60cdf58cb8371913f1efb82
Ajaysetti <ajay.kumar101114@gmail.com>
2020-07-01 19:55:07 +0530

    project created

---
+++ Assets/Hacker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowTheMenu();
+    }
+    void ShowTheMenu()
+    {
+        Terminal.WriteLine("welcome to jumblings");
+        Terminal.WriteLine("select the game difficulty");
+        Terminal.WriteLine("Easy");
+        Terminal.WriteLine("Medium");
+        Terminal.WriteLine("Difficult");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
